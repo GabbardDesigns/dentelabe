c49341c368ee64765850ae2aa68b76f318e9a593
Patrick Gabbard <gabbbarddesigns@gmail.com>
2020-11-16 00:45:30 -0500

    Added background music, sound efffects, socring and start button)

--- js/main.js
+++ js/main.js
@@ -1,28 +1,45 @@
 const canvas = document.getElementById('main_canvas');
 const ctx = canvas.getContext('2d');
+const backgroundMusic = document.getElementById("backgroundMusic");
+backgroundMusic.loop = true;
+//backgroundMusic.addEventListener("playing", button. , false);
+// backgroundMusic.addEventListener("pause", stop, false);
+// backgroundMusic.addEventListener("ended", stop, false);
+let hurtSound = document.getElementById("hurt");
+let deadSound = document.getElementById("dead");
 
-   let width = canvas.width = window.innerWidth;
-   let height = canvas.height = window.innerHeight;
+
+let timer = 0;
+let width = canvas.width = window.innerWidth;
+let height = canvas.height = window.innerHeight;
+let startBtn = document.getElementById("startBtn");
 
 window.addEventListener('resize', function(){
    width = canvas.width = window.innerWidth;
    height = canvas.height = window.innerHeight;
+   player.y = .8 * height;
 })
 
+
+function stop() {
+     clearTimeout(timer);
+}
+
+let survived = 0;
 
 
  // const width = canvas.width = 800;
  // const height = canvas.height = 500;
  const candyList = [];
  const keys = [];
- const level =2;
+ const level =1;
 
  let currentEnemyMax = 1
- const enemyMax = 2 * level
+ const enemyMax = 8*level;
 
  const player = {
-   x: 200,
-   y: 200,
+   x: .5*width,
+   y: .9*height,
    width: 33,
    height: 45,
    frameX: 1,
@@ -31,7 +48,8 @@ window.addEventListener('resize', function(){
    moving: false,
    hitcount: 0,
    collisionImmume: 0,
-   immuneStopTime: Date.now()
+   immuneStopTime: Date.now(),
+   blinking: false
  }
 
  const playerSprite = new Image();
@@ -53,7 +71,6 @@ function movePlayer(){
   if (keys['ArrowDown'] || keys['ArrowUp'] || keys['ArrowLeft'] || keys['ArrowRight']) {
      console.log(keys);
      player.moving= true;
-     console.log("poopty pants");
 
     // if (keys['ArrowUp'] && player.y > 0) {
     //   player.y -= player.speed;
@@ -67,12 +84,12 @@ function movePlayer(){
 
     if (keys['ArrowLeft'] && player.x > 0) {
       player.x -= player.speed;
-      player.frameY = 0+ player.hitcount;
+      player.frameY = player.hitcount*2;
     }
 
     if (keys['ArrowRight'] && player.x < canvas.width - player.width) {
       player.x += player.speed;
-      player.frameY = 1 + player.hitcount;
+      player.frameY = 1 + player.hitcount*2;
     }
   }
   else {
@@ -120,12 +137,11 @@ window.addEventListener("keyup", function (e){
 
  let fps, fpsInterval, startTime, now, then, elapsed;
 
- function startAnimating(fps = 2){
-   fpsInterval = 100000/fps;
+ function startAnimating(fps = 60){
+   fpsInterval = 1000/fps;
    then = Date.now();
    startTime = then;
    animate();
-
  }
 
 function animate(){
@@ -134,22 +150,22 @@ function animate(){
    elapsed = now - then;
    if (elapsed > fpsInterval){
      then = now - (elapsed % fpsInterval);
-   }
-   //ctx.drawImage(background, 0,0, canvas.width, canvas.height);
-   ctx.clearRect(0,0,canvas.width, canvas.height);
 
-   drawSprite(playerSprite,player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height )
-   drawCandy();
-   movePlayer();
-   if (player.collisionImmume===0) {
-        collisionDetection();
-   } else {
-     checkImmuneValidity();
-   }
-
-   // handlePlayerFrame();
-}
-
+     //ctx.drawImage(background, 0,0, canvas.width, canvas.height);
+     ctx.clearRect(0,0,canvas.width, canvas.height);
+     ctx.font = 'bold 20px Open Sans';
+     ctx.textAlign = 'center';
+     ctx.fillText(`Survived ${survived} of ${enemyMax * 5}`, .3*width,50)
+     ctx.fillText(`Score: ${survived * 650}`, .7*width,50)
+     let frequency = 200;
+      if (! player.blinking || Math.floor(Date.now() / frequency) % 2) {
+          drawSprite(playerSprite,player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height )
+      }
+     movePlayer();
+     drawCandy();
+     // handlePlayerFrame();
+  }
+ }
 
 function drawCandy() {
 
@@ -169,15 +185,26 @@ function drawCandy() {
      //moveCandy()
      candy.y += candy.speed;
 
+     candy.rotation+= 6;
+
       //checkOffScreen()
      if (candy.y > height) {
         candy.x = (Math.random() * width ) - candy.width
         candy.y = Math.random() * height - height
         candy.speed = (4 * Math.random()) + 2
+        survived += 1;
      }
 
      let candySprite = new Image();
      candySprite.src = candy.candySpriteSRC;
+
+     if (between(player.x, candy.x, candy.x + candy.width)){
+          if  (player.collisionImmume===0) {
+            collisionDetection(candy.y, candy.height);
+           } else {
+             checkImmuneValidity();
+           }
+      }
      drawSprite(candySprite, candy.frameX * candy.width, candy.frameY * candy.height, candy.width, candy.height, candy.x, candy.y, candy.width, candy.height)
 
     }
@@ -207,6 +234,7 @@ function drawCandy() {
        speed: (4 * Math.random()) +2,
        moving: false,
        id: candyList.length,
+       rotation: 360 * Math.random(),
      };
 
      candyList[candyList.length] = candy;
@@ -222,34 +250,91 @@ function sleep(resolve, ms) {
 
 
 function gameLoop(){
-//    while (!gameOver) {
-//     checkInputs()
-//     gameUpdate()
-//     showGameUpdates()
-// }
+//   while (!gameOver) {
+//      checkInputs()
+//      gameUpdate()
+//      showGameUpdates()
+// // }
+   startBtn.style.display = 'none';
+   backgroundMusic.play();
    startAnimating();
 
 }
 
-gameLoop();
+//gameLoop();
 
-function collisionDetection() {
-    candyList.forEach(function(candy) {
-      if(candy.x > player.x && candy.x < player.x+player.width && candy.y >= player.y && candy.y <player.y+player.height) {
+function collisionDetection(candyStart, candyEnd ) {
+      if(between(candyStart+ candyEnd , player.y,  player.y +  player.height)){
                 console.log("Ouch!");
-                   setTimeout(takeAHit(), 10000);
+                takeAHit();
+
             }
-        })
+
 }
 
 function takeAHit() {
-  player.hitcount += 2;
-  player.immuneStopTime= Date.now()+3000;
+  hurtSound.play();
+  player.hitcount += 1;
+  if (player.hitcount < 4) {
+  player.immuneStopTime= Date.now()+1000;
   player.collisionImmume = 1;
+  player.frameY = player.hitcount*2;
+  player.blinking = true}
+  else {
+    deadSound.play();
+    gameOver = true;
+    backgroundMusic.pause();
+    backgroundMusic.currentTime= 0;
+    player.frameX = 9;
+  }
 }
 
 function checkImmuneValidity() {
   if (Date.now() > player.immuneStopTime ){
       player.collisionImmume = 0;
+      player.blinking = false
   }
 }
+
+function between(x, min, max) {
+  return x >= min && x <= max;
+}
+
+
+
+
+
+
+// Credit this piece by copying the following to your credits section:
+//
+// "Frogs Legs Rag" Kevin MacLeod (incompetech.com)
+// Licensed under Creative Commons: By Attribution 4.0 License
+// http://creativecommons.org/licenses/by/4.0/
+
+//
+
+// Credit this piece by copying the following to your credits section:
+//
+// "Getting it Done" Kevin MacLeod (incompetech.com)
+// Licensed under Creative Commons: By Attribution 4.0 License
+// http://creativecommons.org/licenses/by/4.0
+
+//
+// Wholesome by Kevin MacLeod
+// Link: https://incompetech.filmmusic.io/song/5050-wholesome
+// License: http://creativecommons.org/licenses/by/4.0/
+
+//
+// Two Finger Johnny by Kevin MacLeod
+// Link: https://incompetech.filmmusic.io/song/5008-two-finger-johnny
+// License: http://creativecommons.org/licenses/by/4.0/
+
+//
+// Ditty Pong by Kevin MacLeod
+// Link: https://incompetech.filmmusic.io/song/4676-ditty-pong
+// License: http://creativecommons.org/licenses/by/4.0/
+
+//
+// Silly Fun by Kevin MacLeod
+// Link: https://incompetech.filmmusic.io/song/4360-silly-fun
+// License: http://creativecommons.org/licenses/by/4.0/
