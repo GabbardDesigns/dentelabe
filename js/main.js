df56623f0d004ea7d05c5f5fe73154a9cabb708f
Patrick Gabbard <gabbbarddesigns@gmail.com>
2020-11-17 23:16:25 -0500

    refactored verbose functions, added pause and gameloop

--- js/main.js
+++ js/main.js
@@ -2,17 +2,21 @@ const canvas = document.getElementById('main_canvas');
 const ctx = canvas.getContext('2d');
 const backgroundMusic = document.getElementById("backgroundMusic");
 backgroundMusic.loop = true;
-//backgroundMusic.addEventListener("playing", button. , false);
-// backgroundMusic.addEventListener("pause", stop, false);
-// backgroundMusic.addEventListener("ended", stop, false);
 let hurtSound = document.getElementById("hurt");
 let deadSound = document.getElementById("dead");
+const reSize = document.getElementById('main_canvas');
 
-
+let firstRun = false;
 let timer = 0;
+
+var KEY_LEFT = 37;
+var KEY_RIGHT = 39;
+var KEY_SPACE = 32;
+
+let startBtn = document.getElementById("startBtn");
+
 let width = canvas.width = window.innerWidth;
 let height = canvas.height = window.innerHeight;
-let startBtn = document.getElementById("startBtn");
 
 window.addEventListener('resize', function(){
    width = canvas.width = window.innerWidth;
@@ -20,107 +24,160 @@ window.addEventListener('resize', function(){
    player.y = .8 * height;
 })
 
-
 function stop() {
      clearTimeout(timer);
 }
 
-let survived = 0;
-
+ const level =1;
+ const enemyMax = 8*level;
 
  // const width = canvas.width = 800;
  // const height = canvas.height = 500;
  const candyList = [];
  const keys = [];
- const level =1;
 
- let currentEnemyMax = 1
- const enemyMax = 8*level;
-
- const player = {
+let widthMulti = 1;
+let heightMulti = 1;
+let gameOver = false;
+let gamePaused = false;
+let currentEnemyMax = 1
+let survived = 0;
+let player = {
    x: .5*width,
    y: .9*height,
-   width: 33,
-   height: 45,
+   width: 66,
+   height: 90,
    frameX: 1,
    frameY: 1,
-   speed: 2,
+   speed: 5,
    moving: false,
    hitcount: 0,
    collisionImmume: 0,
    immuneStopTime: Date.now(),
    blinking: false
  }
-
+let rightWall = canvas.width - player.width;
  const playerSprite = new Image();
- playerSprite.src = "img\\metateeth.png"
- // const background = new Image();
- // background.src = "img\\background.png"
+ playerSprite.src = "img\\allteeth2.png"
+ const background = new Image();
+ background.src = "img\\splash.png"
+
+
+// window.addEventListener('keydown', function (e){
+//   let code =""
+//   if (e.key !== undefined) {
+//     code = e.key;
+//   } else if (e.keyIdentifier !== undefined) {
+//     code = e.keyIdentifier;
+//   } else if (e.keyCode !== undefined) {
+//     code = e.keyCode;
+//   }
+//   keys[code] = true;
+//
+//  });
+
+
+ window.addEventListener("keydown", function(event) {
+  if (event.defaultPrevented) {
+    return; // Do nothing if event already handled
+  }
+
+  switch(event.code) {
+    case "KeyA":
+    case "ArrowLeft":
+      // Handle "left"
+       if (player.x > player.width) {
+         player.x -= player.speed;
+         player.frameY = 1 + player.hitcount * 2;
+         updatePlayerFrame();
+       }
+      break;
+    case "KeyD":
+    case "ArrowRight":
+      // Handle "right"
+     if (player.x < canvas.width - player.width) {
+         player.x += player.speed;
+         player.frameY = player.hitcount * 2;
+         updatePlayerFrame();
+       }
+      break;
+    case "KeyP":
+    case " ":
+      // Toggles Paused State
+      pauseGame();
+      break;
+  }
+
+//  refresh();
+
+  // Consume the event so it doesn't get handled twice
+  //event.preventDefault();
+}, true);
+
+// window.addEventListener("keyup", function (e){
+//   delete keys[e.key];
+//   player.moving = false;
+//  });
+
+
 
  function drawSprite (img, sX, sY, sw,sH, dX, dY, dW, dH){
-   ctx.drawImage(img, sX, sY, sw,sH, dX, dY, dW, dH);
-   handlePlayerFrame();
+   ctx.drawImage(img, sX, sY, sw*widthMulti,sH*heightMulti, dX, dY, dW, dH);
+
  }
 
  function randomIntFromInterval(min, max) { // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function movePlayer(){
-
-  if (keys['ArrowDown'] || keys['ArrowUp'] || keys['ArrowLeft'] || keys['ArrowRight']) {
-     console.log(keys);
-     player.moving= true;
-
-    // if (keys['ArrowUp'] && player.y > 0) {
-    //   player.y -= player.speed;
-    //   player.frameY = 3;
-    // }
-
-    // if (keys['ArrowDown'] && player.y < canvas.height - player.height) {
-    //   player.y += player.speed;
-    //   player.frameY = 0;
-    // }
-
-    if (keys['ArrowLeft'] && player.x > 0) {
-      player.x -= player.speed;
-      player.frameY = player.hitcount*2;
-    }
-
-    if (keys['ArrowRight'] && player.x < canvas.width - player.width) {
-      player.x += player.speed;
-      player.frameY = 1 + player.hitcount*2;
-    }
-  }
-  else {
-    player.moving = false;
-  }
-
-
+function pauseGame() {
+    gamePaused = !gamePaused; // toggle the gamePaused value (false <-> true)
+    if (!gamePaused) startAnimating() // restart loop
 }
+//
+// function checkInput() {
+//   if (keys['ArrowLeft'] || keys['ArrowRight'] || keys['Space'])
+//   {
+//     console.log(keys);
+//     player.moving = true;
+//
+//     // if (keys['ArrowUp'] && player.y > 0) {
+//     //   player.y -= player.speed;
+//     //   player.frameY = 3;
+//     // }
+//
+//     // if (keys['ArrowDown'] && player.y < canvas.height - player.height) {
+//     //   player.y += player.speed;
+//     //   player.frameY = 0;
+//     // }
+//
+//     if (keys['ArrowLeft'] && player.x > 0) {
+//       player.x -= player.speed;
+//       player.frameY = player.hitcount * 2;
+//        updatePlayerFrame();
+//     }
+//
+//     if (keys['ArrowRight'] && player.x < canvas.width - player.width) {
+//       player.x += player.speed;
+//       player.frameY = 1 + player.hitcount * 2;
+//        updatePlayerFrame();
+//     }
+//
+//     if (keys['ArrowUp']) {
+//       pauseGame();
+//     }
+//
+//   }else {
+//       player.moving = false;
+//     }
+// }
 
-function handlePlayerFrame(){
-  if (player.frameX <3 && player.moving) player.frameX ++
+function updatePlayerFrame(){
+  if (player.frameX <3 ) player.frameX ++
   else player.frameX = 0;
 }
 
-window.addEventListener('keydown', function (e){
-  let code =""
-  if (e.key !== undefined) {
-    code = e.key;
-  } else if (e.keyIdentifier !== undefined) {
-    code = e.keyIdentifier;
-  } else if (e.keyCode !== undefined) {
-    code = e.keyCode;
-  }
-  keys[code] = true;
-  console.log(code);
- });
 
-window.addEventListener("keyup", function (e){
-  delete keys[e.key];
-  player.moving = false;
- });
 
 
 /*
@@ -129,7 +186,7 @@ window.addEventListener("keyup", function (e){
    ctx.clearRect(0,0,canvas.width, canvas.height);
    drawSprite(playerSprite,player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height )
    movePlayer();
-   handlePlayerFrame();
+   updatePlayerFrame();
    requestAnimationFrame(animate);
  }
  animate();
@@ -141,43 +198,55 @@ window.addEventListener("keyup", function (e){
    fpsInterval = 1000/fps;
    then = Date.now();
    startTime = then;
+
    animate();
  }
 
 function animate(){
-   requestAnimationFrame(animate);
-   now = Date.now();
-   elapsed = now - then;
-   if (elapsed > fpsInterval){
-     then = now - (elapsed % fpsInterval);
+  //  while (!gameOver) {
+      if (gamePaused) return;
+      ctx.scale(widthMulti, heightMulti)
+      requestAnimationFrame(animate);
+      if (canvas.width !== reSize.width || canvas.height !== reSize.height) {
+        widthMulti = reSize.width/canvas.width ;
+        heightMulti = reSize.height/canvas.height ;
 
-     //ctx.drawImage(background, 0,0, canvas.width, canvas.height);
-     ctx.clearRect(0,0,canvas.width, canvas.height);
-     ctx.font = 'bold 20px Open Sans';
-     ctx.textAlign = 'center';
-     ctx.fillText(`Survived ${survived} of ${enemyMax * 5}`, .3*width,50)
-     ctx.fillText(`Score: ${survived * 650}`, .7*width,50)
-     let frequency = 200;
-      if (! player.blinking || Math.floor(Date.now() / frequency) % 2) {
-          drawSprite(playerSprite,player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height )
+        // canvas.width = reSize.width;
+        // canvas.height = reSize.height;
       }
-     movePlayer();
-     drawCandy();
-     // handlePlayerFrame();
-  }
- }
 
-function drawCandy() {
+      now = Date.now();
+      elapsed = now - then;
+      if (elapsed > fpsInterval) {
+        then = now - (elapsed % fpsInterval);
+        // checkInput();
+        //ctx.drawImage(background, 0,0, canvas.width, canvas.height);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.font = 'bold 20px Open Sans';
+        ctx.textAlign = 'center';
+        ctx.fillText(`Survived ${survived} of ${enemyMax * 5}`, .3 * width, 50)
+        ctx.fillText(`Score: ${survived * 650}`, .7 * width, 50)
+        let frequency = 200;
+        if (!player.blinking || Math.floor(Date.now() / frequency) % 2) {
+          drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height)
+        }
+        // checkInput();
+        drawCandy();
 
-  candyList.forEach((candy) => updateCandy(candy))
+      }
 
-  if (candyList.length < enemyMax) {
-    setTimeout(20000, currentEnemyMax += 1)
-    if (candyList.length < currentEnemyMax) {
-      createCandy(randomIntFromInterval(0, 4))
     }
 
-  }
+//initilize candy pool
+
+  while (candyList.length < enemyMax) {
+          createCandy(randomIntFromInterval(0, 4))
+    }
+
+ //}
+
+function drawCandy() {
+  candyList.forEach((candy) => updateCandy(candy))
 }
 
  function updateCandy(candy){
@@ -198,26 +267,35 @@ function drawCandy() {
      let candySprite = new Image();
      candySprite.src = candy.candySpriteSRC;
 
-     if (between(player.x, candy.x, candy.x + candy.width)){
-          if  (player.collisionImmume===0) {
-            collisionDetection(candy.y, candy.height);
-           } else {
+      if  (player.collisionImmume===0) {
+            if (between(player.x, candy.x, candy.x + candy.width)){
+                  collisionDetection(candy.y, candy.height, player.y, player.height);
+           } else if (between(player.y, candy.y, candy.y + candy.height)){
+                  collisionDetection(candy.x, candy.width, player.x, player.width);
+           }
+            else if (between(candy.x, player.x, player.x + player.width)){
+                  collisionDetection(player.y, player.height, candy.y, candy.height);
+           } else if (between(candy.y, player.y, player.y + player.height)){
+                  collisionDetection(player.x, player.width, candy.x, candy.width);
+           }
+      } else {
              checkImmuneValidity();
            }
-      }
+
+
      drawSprite(candySprite, candy.frameX * candy.width, candy.frameY * candy.height, candy.width, candy.height, candy.x, candy.y, candy.width, candy.height)
 
     }
 
  function createCandy( id =2){
    if (id === 1){
-      buildCandy("img\\redc.png", 50, 50, 0, 0)
+      buildCandy("img\\redc.png", 50, 50, 0, 0, 'redC')
     }
     else if (id === 2){
-      buildCandy("img\\spear.png", 50, 50, 0, 0)
+      buildCandy("img\\spear.png", 50, 50, 0, 0, 'spearmint')
    }
    else if (id === 3){
-       buildCandy("img\\gumdrop.png", 50, 50, 0, 0)
+       buildCandy("img\\gumdrop.png", 50, 50, 0, 0, 'gumdrop')
    }
 
    function buildCandy( path, w, h, frameX, frameY)
@@ -250,24 +328,34 @@ function sleep(resolve, ms) {
 
 
 function gameLoop(){
-//   while (!gameOver) {
-//      checkInputs()
-//      gameUpdate()
-//      showGameUpdates()
-// // }
+ //while(!gameOver) {
    startBtn.style.display = 'none';
+   canvas.style.display = '';
    backgroundMusic.play();
    startAnimating();
-
+// }
 }
 
-//gameLoop();
+gameLoop();
 
-function collisionDetection(candyStart, candyEnd ) {
-      if(between(candyStart+ candyEnd , player.y,  player.y +  player.height)){
+function deadState(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.font="30px Helvetica";
+    ctx.fillStyle = '#ffffff';
+    ctx.textBaseline="middle";
+    ctx.drawImage(background, 0,0, canvas.width, canvas.height);
+    ctx.textAlign="center";
+    ctx.fillText("Space Invaders: Recurse Exercise", canvas.width / 2, canvas.height/2 - 40);
+    ctx.font="16px Arial";
+    drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
+    ctx.fillText("Press 'Space' or touch to start.", canvas.width / 2, canvas.height/2);
+    pauseGame()
+}
+
+function collisionDetection(candyStart, candyEnd , playerStart, playerEnd) {
+      if(between(candyStart+ candyEnd , playerStart,  playerStart +  playerEnd)){
                 console.log("Ouch!");
                 takeAHit();
-
             }
 
 }
@@ -277,15 +365,18 @@ function takeAHit() {
   player.hitcount += 1;
   if (player.hitcount < 4) {
   player.immuneStopTime= Date.now()+1000;
-  player.collisionImmume = 1;
+  player.collisionImmume = 3;
   player.frameY = player.hitcount*2;
   player.blinking = true}
   else {
     deadSound.play();
     gameOver = true;
     backgroundMusic.pause();
-    backgroundMusic.currentTime= 0;
-    player.frameX = 9;
+   // backgroundMusic.currentTime= 0;
+    player.frameX = 8;
+   // debugger;
+
+    deadState()
   }
 }
 
@@ -338,3 +429,25 @@ function between(x, min, max) {
 // Silly Fun by Kevin MacLeod
 // Link: https://incompetech.filmmusic.io/song/4360-silly-fun
 // License: http://creativecommons.org/licenses/by/4.0/
+
+
+// read the local play log file
+parse
+
+
+
+// const csv = require('csv-parser');
+// const fs = require('fs');
+//
+// fs.createReadStream('data\\playrecord.csv')
+//   .pipe(csv())
+//   .on('data', (row) => {
+//     console.log(row);
+//   })
+//   .on('end', () => {
+//     console.log('CSV file successfully processed');
+//   });
+
+
+// connect to API URL (gabbarddesigns.com/gameoptions.csv)
+// create request
--- js/other.js
+++ js/other.js
@@ -1,25 +1,73 @@
-var canvas = document.querySelector("canvas")
-var context = canvas.getContext("2d");
-var player = {width: 30, height: 30, x: 0, y:110, speedX: 0.0, speedY: 0.0};
+// Set constants
+const canvas = document.getElementById('main_canvas');
+const ctx = canvas.getContext('2d');
+const backgroundMusic = document.getElementById("backgroundMusic");
+const background = new Image();
 
-function updateGameArea() {
-    context.clearRect(0, 0, canvas.width, canvas.height)
-    context.fillRect(player.x, player.y, player.width, player.height);
 
-    requestAnimationFrame(updateGameArea);
+background.src = "img\\toothpain.svg"
+backgroundMusic.loop = true;
+
+let width = canvas.width = window.innerWidth;
+let height = canvas.height = window.innerHeight;
+
+window.addEventListener('resize', function(){
+   width = canvas.width = window.innerWidth;
+   height = canvas.height = window.innerHeight;
+   player.y = .8 * height;
+})
+
+
+let startBtn = document.getElementById("startBtn");
+
+
+
+function splashIO (event) {  // function to start the game when IO is correct
+    // check for the correct events
+    if(event.type === "click" || (event.type === "keydown" && event.code === "Enter")){
+         // remove events
+         canvas.removeEventListener("click",splashIO);
+         canvas.removeEventListener("keydown",splashIO);
+         startBtn.style.display = 'none';
+         gameStates.current = gameStates.startGame;
+    }
 }
 
-window.onkeydown = function(event) {
-  event.preventDefault() // stops the button scrolling the page
-  if(event.code == 40) { // down
-    player.y += 10
-  } else if(event.code == 38) { // up
-    player.y -= 10
-  } else if(event.code == 39) { // right
-    player.x += 10
-  } else if(event.code == 37) { // left
-    player.x -= 10
-  }
+
+// holds the game state and game state functions
+gameStates.prototype = {
+     init : undefined,
+     splash: function () {  // display splash ===================================
+         // display splash and wait for new state
+       backgroundMusic.play();
+       ctx.drawImage(background, 0,0, canvas.width, canvas.height);
+//       ctx.clearRect(0,0,canvas.width, canvas.height);
+
+     },
+     setupSplash: function () { // setup splash screen ==========================
+         canvas.addEventListener("click", splashIO);
+         canvas.addEventListener("keydown", splashIO);
+         gameStates.current = gameStates.splash();
+         gameStates['current'](); // call the first frame
+     },
+     startGame: function () { // setup game =====================================
+         gameStates.current = gameStates.game(); //set up state function
+         gameStates['current']();  // call the first frame
+     },
+     game: function () {  // plays the main game  ===============================
+          let red =2+5
+     }
 }
 
-requestAnimationFrame(updateGameArea);
+// main animation loop
+function mainLoop () {
+    gameStates.current(); // run current game state
+    requestAnimationFrame(mainLoop);
+}
+
+gameStates.current = gameStates.setupSplash; // set current state to splash screen
+
+// wait for page to load then start the animation
+window.addEventListener('load', function () {
+    requestAnimationFrame(mainLoop); // start the animation
+})
