e2ad6fa91d6a371e0bbb6c745460977856301e1e
Patrick Gabbard <gabbbarddesigns@gmail.com>
2020-11-19 01:36:21 -0500

    added code louisville specific pieces, form, cookies, regex

--- js/main.js
+++ js/main.js
@@ -5,44 +5,77 @@ backgroundMusic.loop = true;
 let hurtSound = document.getElementById("hurt");
 let deadSound = document.getElementById("dead");
 const reSize = document.getElementById('main_canvas');
+let canvasHolder= document.getElementById('canvasHolder');
+let allaudio = document.getElementsByTagName('audio');
+let silenced = true;
+const modal = document.getElementById('modal');
+const overlay = document.getElementById('overlay');
 
-let firstRun = false;
+ let fps, fpsInterval, startTime, now, then, elapsed;
+
+function welcomeForm() {
+     newGame.style.display = 'none';
+     welcome.style.display = 'unset';
+
+}
+
+function soundToggle() {
+
+  switch (silenced) {
+    case true:
+                for (var j = 0; j < allaudio.length; j++) {
+                      allaudio[j].muted = false;
+                    };
+
+                soundButton.src = "img\\sound.png";
+                break;
+    case false:
+                for (var j = 0; j < allaudio.length; j++) {
+                    allaudio[j].muted = true;
+                  }
+                soundButton.src = "img\\soundoff.png";
+                break;
+
+  }
+  silenced = !silenced;
+
+}
+
 let timer = 0;
-
-var KEY_LEFT = 37;
-var KEY_RIGHT = 39;
-var KEY_SPACE = 32;
 
 let startBtn = document.getElementById("startBtn");
 
 let width = canvas.width = window.innerWidth;
-let height = canvas.height = window.innerHeight;
+let height = canvas.height =  window.innerWidth;
 
 window.addEventListener('resize', function(){
+   pauseGame()
    width = canvas.width = window.innerWidth;
    height = canvas.height = window.innerHeight;
-   player.y = .8 * height;
+   player.y = .5 * width;
+   player.x = .5 * height;
 })
 
 function stop() {
      clearTimeout(timer);
 }
 
- const level =1;
- const enemyMax = 8*level;
+ let level;
+ let enemyMax;
 
- // const width = canvas.width = 800;
- // const height = canvas.height = 500;
- const candyList = [];
+ let candyList = [];
  const keys = [];
 
-let widthMulti = 1;
-let heightMulti = 1;
-let gameOver = false;
-let gamePaused = false;
-let currentEnemyMax = 1
-let survived = 0;
-let player = {
+function setupGame()
+{
+  enemyMax = 8*level;
+  level = 1;
+  candyList = [];
+  gameOver = false;
+  gamePaused = false;
+  survived = 0;
+  currentEnemyMax = 1;
+  player = {
    x: .5*width,
    y: .9*height,
    width: 66,
@@ -55,27 +88,29 @@ let player = {
    collisionImmume: 0,
    immuneStopTime: Date.now(),
    blinking: false
- }
+  }
+}
+
+let name, email, phone, version;
+let currentEnemyMax = 1
+let widthMulti = 1;
+let heightMulti = 1;
+let gameOver;
+let gamePaused;
+let survived;
+
+player = {
+  width: 66,
+  height: 90,
+}
+
 let rightWall = canvas.width - player.width;
  const playerSprite = new Image();
  playerSprite.src = "img\\allteeth2.png"
  const background = new Image();
  background.src = "img\\splash.png"
-
-
-// window.addEventListener('keydown', function (e){
-//   let code =""
-//   if (e.key !== undefined) {
-//     code = e.key;
-//   } else if (e.keyIdentifier !== undefined) {
-//     code = e.keyIdentifier;
-//   } else if (e.keyCode !== undefined) {
-//     code = e.keyCode;
-//   }
-//   keys[code] = true;
-//
-//  });
-
+ const dead = new Image();
+ dead.src = "img\\dead.png";
 
  window.addEventListener("keydown", function(event) {
   if (event.defaultPrevented) {
@@ -88,7 +123,7 @@ let rightWall = canvas.width - player.width;
       // Handle "left"
        if (player.x > player.width) {
          player.x -= player.speed;
-         player.frameY = 1 + player.hitcount * 2;
+         player.frameY = player.hitcount * 2;
          updatePlayerFrame();
        }
       break;
@@ -97,7 +132,7 @@ let rightWall = canvas.width - player.width;
       // Handle "right"
      if (player.x < canvas.width - player.width) {
          player.x += player.speed;
-         player.frameY = player.hitcount * 2;
+         player.frameY = 1+ player.hitcount * 2;
          updatePlayerFrame();
        }
       break;
@@ -108,22 +143,10 @@ let rightWall = canvas.width - player.width;
       break;
   }
 
-//  refresh();
-
-  // Consume the event so it doesn't get handled twice
-  //event.preventDefault();
 }, true);
-
-// window.addEventListener("keyup", function (e){
-//   delete keys[e.key];
-//   player.moving = false;
-//  });
-
-
 
  function drawSprite (img, sX, sY, sw,sH, dX, dY, dW, dH){
    ctx.drawImage(img, sX, sY, sw*widthMulti,sH*heightMulti, dX, dY, dW, dH);
-
  }
 
  function randomIntFromInterval(min, max) { // min and max included
@@ -134,6 +157,7 @@ function pauseGame() {
     gamePaused = !gamePaused; // toggle the gamePaused value (false <-> true)
     if (!gamePaused) startAnimating() // restart loop
 }
+
 //
 // function checkInput() {
 //   if (keys['ArrowLeft'] || keys['ArrowRight'] || keys['Space'])
@@ -192,7 +216,7 @@ function updatePlayerFrame(){
  animate();
 */
 
- let fps, fpsInterval, startTime, now, then, elapsed;
+
 
  function startAnimating(fps = 60){
    fpsInterval = 1000/fps;
@@ -238,8 +262,8 @@ function animate(){
     }
 
 //initilize candy pool
-
-  while (candyList.length < enemyMax) {
+p
+  while (candyList.length <= enemyMax) {
           createCandy(randomIntFromInterval(0, 4))
     }
 
@@ -332,23 +356,30 @@ function gameLoop(){
    startBtn.style.display = 'none';
    canvas.style.display = '';
    backgroundMusic.play();
-   startAnimating();
-// }
+   init();
+ }
+
+function init() {
+  setupGame();
+  startAnimating();
 }
 
-gameLoop();
+// gameLoop();
 
 function deadState(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(dead, 0,0, canvas.width, canvas.height);
     ctx.font="30px Helvetica";
     ctx.fillStyle = '#ffffff';
     ctx.textBaseline="middle";
-    ctx.drawImage(background, 0,0, canvas.width, canvas.height);
+    let deathMessage= `Sorry ${name.value} `;
     ctx.textAlign="center";
-    ctx.fillText("Space Invaders: Recurse Exercise", canvas.width / 2, canvas.height/2 - 40);
+    ctx.fillText(deathMessage, canvas.width*.75, canvas.height/3 );
+    ctx.fillText("Space Invaders: ", canvas.width*.75, canvas.height/3 +40 );
     ctx.font="16px Arial";
     drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
     ctx.fillText("Press 'Space' or touch to start.", canvas.width / 2, canvas.height/2);
+
     pauseGame()
 }
 
@@ -431,8 +462,42 @@ function between(x, min, max) {
 // License: http://creativecommons.org/licenses/by/4.0/
 
 
+ function soundToggle() {
+
+  switch (silenced) {
+    case true:
+                for (var j = 0; j < allaudio.length; j++) {
+                      allaudio[j].muted = false;
+                    };
+
+                soundButton.src = "img\\sound.png";
+                break;
+    case false:
+                for (var j = 0; j < allaudio.length; j++) {
+                    allaudio[j].muted = true;
+                  }
+                soundButton.src = "img\\soundoff.png";
+                break;
+
+  }
+  silenced = !silenced;
+
+}
+
+        //  function musicToggle() {
+        //     if (!music) {
+        //         backgroundMusic.play();
+        //         music = 1;
+        //         soundButton.src = "img\\sound.png";
+        //     } else {
+        //         backgroundMusic.pause();
+        //         music = 0;
+        //         soundButton.src = "img\\soundoff.png";
+        //     }
+        // }
+
+
 // read the local play log file
-parse
 
 
 
@@ -451,3 +516,65 @@ parse
 
 // connect to API URL (gabbarddesigns.com/gameoptions.csv)
 // create request
+
+const emailValidation =/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+
+//
+//  function setCookie(cname, cvalue, exdays) {
+//   let d = new Date();
+//   d.setTime(d.getTime() + (exdays*24*60*60*1000));
+//   let expires = "expires="+ d.toUTCString();
+//   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=None;Secure]";
+// }
+//
+// function getCookie(cname) {
+//   let matches = document.cookie.match(new RegExp(
+//     "(?:^|; )" + cname.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+//   ));
+//   return matches ? decodeURIComponent(matches[1]) : undefined;
+// }
+
+function savetoCookies() {
+                name =  document.forms["RegForm"]["Name"];
+                email = document.forms["RegForm"]["eMail"];
+//                phone = document.forms["RegForm"]["Telephone"];
+                version = document.forms["RegForm"]["Version"];
+
+                if (name.value == "") {
+                    window.alert("Please enter your name.");
+                    name.focus();
+                    return false;
+                }
+
+                if (!emailValidation.test(email.value)) {
+                    window.alert(
+                      "Please enter a valid e-mail address.");
+                    email.focus();
+                    return false;
+                }
+
+                // if (phone.value == "") {
+                //     window.alert(
+                //       "Please enter your telephone number.");
+                //     phone.focus();
+                //     return false;
+                // }
+//
+//                  setCookie(name, name.value, 90);
+//                  setCookie(email, email.value, 90);
+// //                 setCookie(phone, phone.value, 90);
+//                  setCookie(version, version.value, 90);
+                 closeModal()
+                 gameLoop()
+              return false;
+            }
+
+
+function closeModal(){
+        modal.style.display = 'none';
+        overlay.style.display = 'none';
+}
+
+function showCredits(){
+
+}
